20fe07f1755083aac30e9d31bd1caca1fa902d12
82109 <82109@DESKTOP-097JKGH>
2021-09-12 02:10:50 +0900

    초기 커밋.

---
+++ bangtal_.cpp
@@ -0,0 +1,20 @@
+﻿#include <bangtal>
+using namespace bangtal;
+
+bool door_mouseCallback(ObjectPtr object, int x, int y, MouseAction action)
+{
+    endGame();
+    return true;
+}
+
+int main()
+{
+    auto room1 = Scene::create("룸1", "images/배경-1.png");
+
+    auto door = Object::create("Images/문-오른쪽-열림.png", room1, 800, 270);
+
+
+    door->setOnMouseCallback(door_mouseCallback);
+
+    startGame(room1);
+}
